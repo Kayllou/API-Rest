5fb4243d8e66b06cd485fcb3bb2fbcb050140c65
Kayo Novais <kayonovais07@.gmail.com>
2024-10-01 23:10:25 -0300

    cont

--- src/app.js
+++ src/app.js
@@ -2,9 +2,23 @@ import express from "express"
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "Peter Pan"
+    },
+    {
+        id: 2,
+        titulo: "Robin Hood"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
-    //req.
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+    });
 
 export default app;
