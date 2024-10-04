9a6eb6e92485d81f1fbb018098a0ae503d700832
Kayo Novais <kayonovais07@.gmail.com>
2024-10-03 22:03:40 -0300

    cont

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://admin:<admin1234>@cluster0.keyxl.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect("mongodb+srv://admin:admin1234@cluster0.keyxl.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
 
     return mongoose.connection;
 };
