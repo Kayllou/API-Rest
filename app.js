c310c558d1111fe87b87f697ff0f977bc50ebea2
Kayllou <156482599+Kayllou@users.noreply.github.com>
2024-10-05 23:10:18 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import LivroController from "./src/controllers/livroController.js";
 
 const app = express();
 app.use(express.json());
@@ -23,10 +24,6 @@ function buscaLivro(id) {
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
-
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-    });
 
     app.get("/livros/:id", (req, res) => {
         const index = buscaLivro(req.params.id);
